53bc210f58670613bbb42d2c5e4d20e23c4a1b01
MichaelHou <Michael_Hou@asus.com>
2016-03-02 17:20:17 +0800

    what. I even forget how to write a delegate and lambda exp. my poor C#

---
+++ ResetSystem/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace ResetSystem
+{
+    public partial class Form1 : Form
+    {
+        public void eventhdl(object a, EventArgs e)
+        {
+            Process.Start("cmd.exe","/k");
+        }
+        Timer tm1 = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            tm1.Interval = 100;
+            //wrong guess
+//            tm1.Tick += new EventHandler(delegate (a,b)=>{});
+            //this.Load += new EventHandler();           
+//            tm1.Tick += new EventHandler(delegate (object,EventArgs)=>{});
+  
+            //right through try...
+           // tm1.Tick += new EventHandler(eventhdl);
+            //tm1.Tick += new EventHandler(delegate(object a, EventArgs b) {
+            //    Console.WriteLine("test0");            
+            //});
+            //tm1.Tick+= new EventHandler((a,b)=>{
+            //    Console.WriteLine("test");
+            //});
+
+            tm1.Tick += new EventHandler((a, b) => {
+                Process.Start("shutdown.exe", "/r /t 0");
+            });
+            tm1.Start();
+        }
+    }
+}
