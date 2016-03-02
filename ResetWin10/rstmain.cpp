dc0ec77ab336d38687e19023256989f3c212d8f5
MichaelHou <Michael_Hou@asus.com>
2016-03-02 17:19:20 +0800

    add write cf9 06 use cpp.but os will auto repair.

--- ResetWin10/rstmain.cpp
+++ ResetWin10/rstmain.cpp
@@ -6,8 +6,10 @@ int main()
 {
 	LoadATSZIoDriver();
 	WORD temp=0;
+	BYTE PortVal=0x06;
 	Read_Mem_Word(0xfffffff0, &temp);
 	printf("%x\n",temp);
+	Write_Io_Byte(0xcf9,&PortVal);
 	getchar();
 
 	RemoveNTDriver();
